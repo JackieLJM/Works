f0d17d8dec01cdd2ff94c038ab6d6daac2c5ebdd
JackieLJM <ljmlionshy@live.com>
2016-09-06 16:08:48 +0800

    seven

---
+++ Webapp-handlebar/webapp/platforms/windows/www/js/header.js
@@ -0,0 +1,7 @@
+﻿/**
+ * Created by aijing on 2016/8/1.
+ */
+
+document.writeln("<header>");
+document.writeln("    <div class=\"fLeft\"><a href=\"#\">back</a></div><div class=\"hname\"></div><div class=\"fRight\"><img src=\"../images/header3.png\"></div>");
+document.writeln("</header>");
