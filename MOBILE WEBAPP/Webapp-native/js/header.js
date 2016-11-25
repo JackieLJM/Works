7c18bab98a57c6afaec332fdf15d83022d0ad8a1
JackieLJM <ljmlionshy@live.com>
2016-11-25 14:06:40 +0800

    thirty

---
+++ MOBILE WEBAPP/Webapp-native/js/header.js
@@ -0,0 +1,7 @@
+/**
+ * Created by aijing on 2016/8/1.
+ */
+
+document.writeln("<header class=\"prelative\">");
+document.writeln("<div class=\"fLeft\"><a href=\"menu1.html\">back</a></div><div class=\"hname\"></div><div class=\"fRight\"><img src=\"images/header2.png\"></div>");
+document.writeln("</header>");
