09f2fc55d74c1607444c7a3e60840bee0479677b
JackieLJM <ljmlionshy@live.com>
2017-09-27 17:20:29 +0800

    h-c-j

--- 2017-9-22/score.js
+++ 2017-9-22/score.js
@@ -1,8 +1,8 @@
-// var canvas = document.createElement('canvas');
-// var ctx = canvas.getContext('2d');
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-// document.body.insertBefore(canvas, document.body.firstChild);
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+document.body.insertBefore(canvas, document.body.firstChild);
 
 // var div=document.createElement('div');
 // div.id='scoreBoard';
@@ -29,6 +29,8 @@ var sumIn = document.getElementById('sum');
 var infoTitle = document.getElementById('infoTitle');
 var menu = document.getElementById('menu');
 
+infoTitle.style.display='none';
+menu.style.display='none';
 document.getElementById('scoreBoard').onclick = function(event) {
     arr[i] = event.target.textContent;
 
@@ -36,22 +38,27 @@ document.getElementById('scoreBoard').onclick = function(event) {
     if (arr[i] === '全零') {
         sum = 0;
         arr[i] = 0;
-        sum = sum + arr[i];
+        // sumIn.value=arr[i];
+        // sum = sum + arr[i];
     } else if (typeof Number(arr[i]) !== NaN) {
         arr[i] = parseFloat(arr[i]);
-        sum = sum + arr[i];
+        // sumIn.value=arr[i];
+        // sum = sum + arr[i];
     } else if (arr[i] === '·') {
-        sum = sum + '.';
-        return sum;
+        arr[i]=0;
     }
+    sumIn.value = arr[i];
     i++;
-    sumIn.value = sum;
 }
 
-document.getElementById('information').onclick = function() {
+document.getElementById('information').onclick = function(event) {
+	event.preventDefault();
     infoTitle.style.display === 'none' ? infoTitle.style.display = 'inline-block' : infoTitle.style.display = 'none';
+    menu.style.display='none';
 }
 
-document.getElementById('more').onclick = function() {
+document.getElementById('more').onclick = function(event) {
+	event.preventDefault();
     menu.style.display === 'none' ? menu.style.display = 'inline-block' : menu.style.display = 'none';
+    infoTitle.style.display='none';
 }
