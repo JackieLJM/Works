fd0fac39936b0642f5dbbbfe70eb369d323e7ede
JackieLJM <ljmlionshy@live.com>
2017-10-09 17:33:42 +0800

    add gulp sprite

--- 2017-9-22/gulpfile.js
+++ 2017-9-22/gulpfile.js
@@ -3,7 +3,9 @@ let gulp=require('gulp'),
 	minihtml=require('gulp-minify-html'),
 	uglify=require('gulp-uglify'),
 	minimage=require('gulp-imagemin'),
-	rename=require('gulp-rename');
+	rename=require('gulp-rename'),
+	spritesmith=require('gulp-spritesmith'),
+	buffer=require('vinyl-buffer');
 
 gulp.task('css',function(){
 	return gulp.src('src/score.css').pipe(minicss()).pipe(gulp.dest('./build'));
@@ -21,5 +23,9 @@ gulp.task('js',function(){
 gulp.task('img',function(){
 	return gulp.src('./img/*.png').pipe(minimage()).pipe(rename({suffix:'.min'})).pipe(gulp.dest('./img'));
 });
+
+gulp.task('sprite',function(){
+	return gulp.src('./img/*.png').pipe(spritesmith({imgName:'logo.png',cssName:'logo.css'})).pipe(buffer()).pipe(gulp.dest('./build'));
+})
 
 gulp.task('default',['css','html','js'],function(){});
